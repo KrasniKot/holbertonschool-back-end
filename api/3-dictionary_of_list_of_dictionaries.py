e682e3ed71dad6807ba9989c848bc894971225a3
KrasniKot <6187@holbertonstudents.com>
2023-08-22 15:30:44 -0700

    task 3: done

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -12,9 +12,18 @@ if __name__ == "__main__":
     root_url = f"https://jsonplaceholder.typicode.com"
 
     users = get(f"{root_url}/users").json()
-    todos = get(f"{root_url}/todos").json()
 
-    new_json = {u.get("id"): [{"username": u.get("username")} ] for u in users}
+    for user in users:
+        USER_ID = user.get("id")
+
+        todos = get(f"{root_url}/todos?userId={USER_ID}").json()
+
+        usr_dicts = [{
+            "username": user.get("username"),
+            "task": todo.get("title"),
+            "completed": todo.get("completed")} for todo in todos]
+
+        new_json[USER_ID] = usr_dicts
 
     with open("todo_all_employees.json", "w") as f:
         dump(new_json, f)
