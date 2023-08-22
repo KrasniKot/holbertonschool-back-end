a34627ca08a9c591356c197acb4d99b6e736ec66
KrasniKot <6187@holbertonstudents.com>
2023-08-22 12:29:34 -0700

    task 1: request ok

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """This module contains the task 0"""
 
-from csv import writer, QUOTE_ALL
+
+import csv
 from requests import get
 from sys import argv
 
 if __name__ == "__main__":
 
     root_url = f"https://jsonplaceholder.typicode.com"
-    NAME = get(f"{root_url}/users/{argv[1]}").json().get("name")
+    USERNAME = get(f"{root_url}/users/{argv[1]}").json().get("username")
 
     request = get(f"{root_url}/todos?userId={argv[1]}").json()
 
     with open(argv[1] + ".csv", "w") as f:
-        w = writer(f, quoting=QUOTE_ALL)
-        for r in request:
-            w.writerow([argv[1], NAME, r.get("completed"), r.get("title")])
+        w = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for i in request:
+            w.writerow([argv[1], USERNAME, i.get("completed"), i.get("title")])
