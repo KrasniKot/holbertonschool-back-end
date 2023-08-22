7f5659da3d4f55172380fbc396ea9c9fd563c43c
KrasniKot <6187@holbertonstudents.com>
2023-08-22 14:56:43 -0700

    task 3: in progress

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -8,13 +8,13 @@ from sys import argv
 
 if __name__ == "__main__":
 
-	new_json = {}
-	root_url = f"https://jsonplaceholder.typicode.com"
+    new_json = {}
+    root_url = f"https://jsonplaceholder.typicode.com"
 
-	users = get(f"{root_url}/users").json()
-	todos = get(f"{root_url}/todos").json()
+    users = get(f"{root_url}/users").json()
+    todos = get(f"{root_url}/todos").json()
 
-	new_json = [user.get("id"): {"idk": user.get("name")} for user in users]
+    new_json = {u.get("id"): [{"username": u.get("username")} ] for u in users}
 
-	with open(argv[1] + ".json", "w") as f:
-        	dump(new_json, f)
+    with open("todo_all_employees.json", "w") as f:
+        dump(new_json, f)
