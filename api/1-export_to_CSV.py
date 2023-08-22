bf28dd5b0e373af713db24378881189d135894f5
KrasniKot <6187@holbertonstudents.com>
2023-08-22 12:00:15 -0700

    task 1: fortmat fixed

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -8,16 +8,11 @@ from sys import argv
 if __name__ == "__main__":
 
     root_url = f"https://jsonplaceholder.typicode.com"
-    EMPLOYEE_NAME = get(f"{root_url}/users/{argv[1]}").json().get("name")
+    NAME = get(f"{root_url}/users/{argv[1]}").json().get("name")
 
     request = get(f"{root_url}/todos?userId={argv[1]}").json()
 
-    with open(f"{argv[1]}.csv", "w") as f:
-        writing = writer(f, quoting=QUOTE_ALL)
-        for result in request:
-            writing.writerow([
-                    argv[1],
-                    EMPLOYEE_NAME,
-                    result["completed"],
-                    result["title"]
-                    ])
+    with open(argv[1] + ".csv", "w") as f:
+        w = writer(f, quoting=QUOTE_ALL)
+        for r in request:
+            w.writerow([argv[1], NAME, r.get("completed"), r.get("title")])
